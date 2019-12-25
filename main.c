0e3714b95049403665b0f5c19616266e6817f2cf
Uliana <uliana@Ivan.proba>
2019-12-25 20:51:58 +0300

    fixed

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <Static.h>
+#include <Dynamic.h>
+
+int main() {
+
+    return 0;
+}
--- project/project.h
+++
@@ -1,7 +0,0 @@
-#ifdef __cplusplus
-extern "C"
-#endif
-
-int Compare_matrix (int**, int**, int, int);
-
-int Check_inital_data (int**, int**, int, int);
